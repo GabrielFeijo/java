a977d8be6bcb7adbcdac5e091e31ffc8c3f3b91b
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2021-10-23 16:25:39 -0300

    Initial commit

---
+++ main.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {   
+        int idade;
+        String maior; 
+         Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite sua idade ");
+        idade = ler.nextInt();
+
+        if(idade > 18){
+          maior = "Você é de maior";
+        }else{
+            maior = "Você é de menor" ;
+        }
+
+        System.out.println(idade);
+        System.out.println(maior);
+    }
+}
