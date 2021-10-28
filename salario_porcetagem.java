020fe239b23862bfbc25aab674b9bfb1630729f1
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2021-10-27 23:43:39 -0300

    Create salario_porcetagem.java

---
+++ salario_porcetagem.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class salario_porcetagem {
+  
+    public static void main(String[] args) { 
+        double valor,porce,total,novo;
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite o valor do seu salário.");
+        valor = ler.nextDouble();
+        System.out.println("Digite a porcetagem de desconto.");
+        porce = ler.nextDouble();
+
+        total = valor*(porce/100);
+        novo = valor - total;
+
+        
+        System.out.printf("O valor de " + porce + " de seu salário é %.2f.%n", total );
+        System.out.printf("O salário no valor de " + valor + "  -  %.2f.%n" , total );
+        System.out.printf("É igual a %.2f.%n" , novo);
+        
+        ler.close();
+
+
+    }
+}
